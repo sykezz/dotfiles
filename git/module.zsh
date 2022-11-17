f2f262438c6fdd398951d74d1d3a3c55ef629fcb
Hussain 'Sykez' Faisal <hello@sykez.com>
2022-11-17 14:20:49 +0800

    git

---
+++ git/module.zsh
@@ -0,0 +1,8 @@
+# Git module aliases
+alias gs="git status"
+alias gpl="git pull"
+alias gp="git push"
+alias gco="git checkout"
+alias gadd="git add ."
+alias gcm="git commit -m"
+alias grst="git reset --hard"
