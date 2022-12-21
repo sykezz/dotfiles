62718ed63a0248301442b151f6478a00edaebd09
Hussain 'Sykez' Faisal <hello@sykez.com>
2022-12-21 15:22:25 +0800

    Ruby 2.7

--- ruby/install.sh
+++ ruby/install.sh
@@ -12,17 +12,12 @@ eval "$(rbenv init -)"
 
 echo "📦 Installing Ruby..."
 # Prequisites
-brew install shared-mime-info openssl@1.1
-export PATH="/opt/homebrew/opt/openssl@1.1/bin:$PATH"
-export LDFLAGS="-L/opt/homebrew/opt/openssl@1.1/lib"
-export CPPFLAGS="-I/opt/homebrew/opt/openssl@1.1/include"
-export PKG_CONFIG_PATH="/opt/homebrew/opt/openssl@1.1/lib/pkgconfig"
-export RUBY_CONFIGURE_OPTS="--with-openssl-dir=/opt/homebrew/opt/openssl@1.1"
+brew install shared-mime-info openssl@3
 
 RUBY_CFLAGS="-Wno-error=implicit-function-declaration" \
-  rbenv install 2.6.10
+  rbenv install 2.7.1
 gem install bundler
-rbenv global 2.6.10
+rbenv global 2.7.1
 rbenv rehash
 
 echo "🧩 Enabling Ruby module..."
